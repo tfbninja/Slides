21ece40448d8f3d19d77af322a7be589fbd30c05
Tim Barber <tfbninja@users.noreply.github.com>
2018-12-04 13:07:31 -0700

    fixed herd classes

--- slides_and_javacode/s18_array_of_refs/Herd.java
+++ slides_and_javacode/s18_array_of_refs/Herd.java
@@ -2,37 +2,36 @@ package s18_array_of_refs;
 
 // A+ Computer Science
 // www.apluscompsci.com
-
 //array of references as instance variable example
-
 import static java.lang.System.*;
 import java.util.Arrays;
 import java.util.Collections;
 
-public class Herd
-{
-	private Creature[] creatureList;
+public class Herd {
 
-	public Herd()
-	{
-		//must size the array		
+    private Creature[] creatureList;
 
-	}		
-	
-	public Herd(int size)
-	{
-		//must size the array		
+    public Herd() {
+        creatureList = new Creature[10];
+    }
 
-	}	
-	
-	public void add(int Herdize)
-	{
-		//add in a new creature of size Herdize
-		
-	}
-	
-	public String toString()
-	{
-		return "" + Arrays.toString(creatureList);
-	}
+    public Herd(int size) {
+        this.creatureList = new Creature[size];
+    }
+
+    public void add(int Herdsize) {
+        int index = 0;
+        for (Creature i : this.creatureList) {
+            if (i == null) {
+                break;
+            } else {
+                index++;
+            }
+        }
+        this.creatureList[index] = new Creature(Herdsize);
+    }
+
+    public String toString() {
+        return "" + Arrays.toString(creatureList);
+    }
 }
