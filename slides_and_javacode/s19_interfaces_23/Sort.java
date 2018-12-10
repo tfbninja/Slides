ebdbf0afae97d7cc5b8c12a9eb2317998c511a1e
Tim Barber <tfbninja@users.noreply.github.com>
2018-12-10 13:02:11 -0700

    updated sort.java

--- slides_and_javacode/s19_interfaces_23/Sort.java
+++ slides_and_javacode/s19_interfaces_23/Sort.java
@@ -1,28 +1,21 @@
 package s19_interfaces_23;
 
-// A+ Computer Science
-// www.apluscompsci.com
-
 //sort example using an array of Comparable
-
-import static java.lang.System.*;
 import java.util.Arrays;
 
-public class Sort
-{
-	public static void main ( String[] args )
-	{
-		Comparable[] creatureList = new Creature[3];
-		//add 3 creatures to creatureList
-		
-		
-		
-		Arrays.sort(creatureList);  		//will throw an exception until
-															//creatures are added to the array		
-		
-		for(Comparable it : creatureList)
-		{
-			out.println(it);
-		}		
-  }
+public class Sort {
+
+    public static void main(String[] args) {
+        Comparable[] creatureList = new Creature[3];
+        creatureList[0] = new Creature(1);
+        creatureList[1] = new Creature(2);
+        creatureList[2] = new Creature(3);
+
+        Arrays.sort(creatureList);  		//will throw an exception until
+        //creatures are added to the array		
+
+        for (Comparable it : creatureList) {
+            System.out.println(it);
+        }
+    }
 }
