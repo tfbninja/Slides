90e3a3a23407069d3fd3e14b7c79286d1f2f6957
Tim Barber <tfbninja@users.noreply.github.com>
2019-01-10 12:46:27 -0700

    aight

--- slides_and_javacode/s20_inheritance/WhatsOnTheInside.java
+++ slides_and_javacode/s20_inheritance/WhatsOnTheInside.java
@@ -1,64 +1,56 @@
 package s20_inheritance;
-
-// A+ Computer Science
-// www.apluscompsci.com
-
 //inheritance example
 
 import static java.lang.System.*;
 
-class Monster
-{
-	private String myName = "long way to go for a toString()";
+class Monster1 {
 
-	public Monster()
-	{
-		//myName = "Monster";    //uncomment and recompile and run
-	}
+    private String myName = "long way to go for a toString()";
 
-	public Monster( String name )
-	{
-		myName = name;
-	}
+    public Monster1() {
+        myName = "Monster reee";    //uncomment and recompile and run
+    }
 
-	public String toString( )
-	{
-		return myName;
-	}
+    public Monster1(String name) {
+        myName = name;
+    }
+
+    public String toString() {
+        return myName;
+    }
 }
 
-class Witch extends Monster
-{
-   public Witch( )	//GoodWitch will not compile without this constructor
-   { 
-   	//automatically calls super()
-   }  
+class Witch1 extends Monster1 {
 
-   public Witch( String name )
-   {
+    public Witch1() //GoodWitch will not compile without this constructor
+    {
+        super();
+    }
 
-   }
+    public Witch1(String name) {
+        super(name);
+    }
 }
 
-class GoodWitch extends Witch
-{
-   //what do we need here??
-   
-   
-   public GoodWitch( String name )
-   {
- 	  //automatically calls super()
-   }
+class GoodWitch extends Witch1 {
+    //what do we need here??
+
+    public GoodWitch() {
+        super();
+    }
+
+    public GoodWitch(String name) {
+        super(name);
+    }
 }
 
-public class WhatsOnTheInside
-{
-	public static void main ( String[] args )
-	{
-		GoodWitch bad = new GoodWitch();		//why does this not compile
-		out.println(bad);
-		
-		GoodWitch witch = new GoodWitch("Harriet");
-		out.println(witch);
-	}
+public class WhatsOnTheInside {
+
+    public static void main(String[] args) {
+        GoodWitch bad = new GoodWitch();		//why does this not compile
+        out.println(bad);
+
+        GoodWitch witch = new GoodWitch("Harriet");
+        out.println(witch);
+    }
 }
